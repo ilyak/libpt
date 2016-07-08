160035a6985f1dc5202a5141a0102e64c7f2f352
Ilya Kaliman <ilya.kaliman@gmail.com>
2016-07-08 10:14:03 -0700

    Add more documentation.

--- pt.h
+++ pt.h
@@ -5,7 +5,17 @@
 
 __BEGIN_DECLS
 
-/* Computes CCSD(T) energy correction. */
+/* Compute CCSD(T) energy correction in parallel.
+ *
+ * o/v - sizes of occupied/virtual spaces
+ * d_ov - Delta matrix (size o*v)
+ * f_ov - Fock matrix (size o*v)
+ * i_ooov - OOOV integrals (size o*o*o*v)
+ * i_oovv - OOVV integrals (size o*o*v*v)
+ * i_ovvv - OVVV integrals (size o*v*v*v)
+ * t1 - CCSD T1 amplitudes (size o*v)
+ * t2 - CCSD T2 amplitudes (size o*o*v*v)
+ */
 double ccsd_pt(size_t o, size_t v, const double *d_ov,
     const double *f_ov, const double *i_ooov, const double *i_oovv,
     const double *i_ovvv, const double *t1, const double *t2);
