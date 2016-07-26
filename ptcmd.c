201eb8d816bdff6034a6229af23c5803d5b3b2c9
Ilya Kaliman <ilya.kaliman@gmail.com>
2016-07-26 11:17:27 -0700

    Adjust to work with sparse interface.

--- ptcmd.c
+++ ptcmd.c
@@ -181,7 +181,91 @@ load_random_data(size_t o, size_t v, double *d_ov,
 	}
 }
 
+#define T2(i, j, a, b) t2[i*o*v*v+j*v*v+a*v+b]
+#define I_OOOV(i, j, k, a) i_ooov[i*o*o*v+j*o*v+k*v+a]
+#define I_OOVV(i, j, a, b) i_oovv[i*o*v*v+j*v*v+a*v+b]
 #define I_OVVV(i, a, b, c) i_ovvv[i*v*v*v+a*v*v+b*v+c]
+
+static void
+convert_t2(size_t o, size_t v, const double *t2, struct st4 *st)
+{
+	size_t i, j, b, c;
+
+	memset(st, 0, sizeof(*st));
+
+	for (i = 0; i < o; i++)
+	for (j = 0; j < o; j++)
+	for (b = 0; b < v; b++)
+	for (c = 0; c < v; c++)
+		if (T2(i, j, b, c) != 0.0) {
+			st->len++;
+			st->idx = xreallocarray(st->idx, st->len,
+			    sizeof(*st->idx));
+			st->data = xreallocarray(st->data, st->len,
+			    sizeof(*st->data));
+			st->idx[st->len-1].a = i;
+			st->idx[st->len-1].b = j;
+			st->idx[st->len-1].c = b;
+			st->idx[st->len-1].d = c;
+			st->data[st->len-1] = T2(i, j, b, c);
+		}
+
+	printf("t2 %zu %zu\n", o*o*v*v, st->len);
+}
+
+static void
+convert_ooov(size_t o, size_t v, const double *i_ooov, struct st4 *st)
+{
+	size_t i, j, k, c;
+
+	memset(st, 0, sizeof(*st));
+
+	for (i = 0; i < o; i++)
+	for (j = 0; j < o; j++)
+	for (k = 0; k < o; k++)
+	for (c = 0; c < v; c++)
+		if (I_OOOV(i, j, k, c) != 0.0) {
+			st->len++;
+			st->idx = xreallocarray(st->idx, st->len,
+			    sizeof(*st->idx));
+			st->data = xreallocarray(st->data, st->len,
+			    sizeof(*st->data));
+			st->idx[st->len-1].a = i;
+			st->idx[st->len-1].b = j;
+			st->idx[st->len-1].c = k;
+			st->idx[st->len-1].d = c;
+			st->data[st->len-1] = I_OOOV(i, j, k, c);
+		}
+
+	printf("i_ooov %zu %zu\n", o*o*o*v, st->len);
+}
+
+static void
+convert_oovv(size_t o, size_t v, const double *i_oovv, struct st4 *st)
+{
+	size_t i, j, b, c;
+
+	memset(st, 0, sizeof(*st));
+
+	for (i = 0; i < o; i++)
+	for (j = 0; j < o; j++)
+	for (b = 0; b < v; b++)
+	for (c = 0; c < v; c++)
+		if (I_OOVV(i, j, b, c) != 0.0) {
+			st->len++;
+			st->idx = xreallocarray(st->idx, st->len,
+			    sizeof(*st->idx));
+			st->data = xreallocarray(st->data, st->len,
+			    sizeof(*st->data));
+			st->idx[st->len-1].a = i;
+			st->idx[st->len-1].b = j;
+			st->idx[st->len-1].c = b;
+			st->idx[st->len-1].d = c;
+			st->data[st->len-1] = I_OOVV(i, j, b, c);
+		}
+
+	printf("i_oovv %zu %zu\n", o*o*v*v, st->len);
+}
 
 static void
 convert_ovvv(size_t o, size_t v, const double *i_ovvv, struct st4 *st)
@@ -207,7 +291,7 @@ convert_ovvv(size_t o, size_t v, const double *i_ovvv, struct st4 *st)
 			st->data[st->len-1] = I_OVVV(i, a, b, c);
 		}
 
-	printf("xxx %zu %zu\n", o*v*v*v, st->len);
+	printf("i_ovvv %zu %zu\n", o*v*v*v, st->len);
 }
 
 int
@@ -218,7 +302,7 @@ main(int argc, char **argv)
 	double *i_ooov, *i_oovv, *i_ovvv;
 	double *t1, *t2;
 	double e_pt, e_ref = 0.0;
-	struct st4 it_ovvv;
+	struct st4 tt2, it_ooov, it_oovv, it_ovvv;
 	int rank;
 	const char *errstr, *testpath = NULL;
 	char ch;
@@ -277,8 +361,12 @@ main(int argc, char **argv)
 	MPI_Bcast(t1, o*v, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 	MPI_Bcast(t2, o*o*v*v, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+	convert_t2(o, v, t2, &tt2);
+	convert_ooov(o, v, i_ooov, &it_ooov);
+	convert_oovv(o, v, i_oovv, &it_oovv);
 	convert_ovvv(o, v, i_ovvv, &it_ovvv);
-	e_pt = ccsd_pt(o, v, d_ov, f_ov, i_ooov, i_oovv, &it_ovvv, t1, t2);
+	e_pt = ccsd_pt(o, v, d_ov, f_ov, t1, &tt2, &it_ooov,
+	    &it_oovv, &it_ovvv);
 	if (rank == 0)
 		printf("ccsd(t) energy: % .8lf\n", e_pt);
 	if (testpath) {
