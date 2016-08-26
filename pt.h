5bdecef3611c134f6e9ff40021a11600acb423fc
Ilya Kaliman <ilya.kaliman@gmail.com>
2016-08-26 11:08:26 -0700

    More comment tweaks.

--- pt.h
+++ pt.h
@@ -32,10 +32,10 @@ extern "C" {
  *   d_ov - Delta matrix (size o*v)
  *   f_ov - Fock matrix (size o*v)
  *   t1 - CCSD T1 amplitudes (size o*v)
- *   t2 - CCSD T2 amplitudes (sparse tensor, full size o*o*v*v)
- *   i_oovo - transposed OOOV integrals (sparse tensor, full size o*o*o*v)
- *   i_oovv - OOVV integrals (sparse tensor, full size o*o*v*v)
- *   i_vvov - transposed OVVV integrals (sparse tensor, full size o*v*v*v)
+ *   t2 - CCSD T2 amplitudes (size o*o*v*v)
+ *   i_oovo - OOOV integrals transposed (size o*o*o*v)
+ *   i_oovv - OOVV integrals (size o*o*v*v)
+ *   i_vvov - OVVV integrals transposed (size o*v*v*v)
  *
  * All arrays should be arranged contiguously in memory by last index first.
  * E.g., for d_ov the first v contiguous elements in memory are d_ov[o=0,v=0],
