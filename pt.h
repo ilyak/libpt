6e4878df813a74f5bdb6487a70dac7774193b2b2
Ilya Kaliman <ilya.kaliman@gmail.com>
2016-09-12 16:20:32 -0700

    Docs.

--- pt.h
+++ pt.h
@@ -26,9 +26,42 @@ extern "C" {
 
 /* Compute CCSD(T) energy correction in parallel.
  *
+ * The routine is MPI/OpenMP parallel. All MPI processes must receive same
+ * input data.
+ *
  * Arguments:
- *   o - size of occupied space
- *   v - size of virtual space
+ *   o - size of spin-block occupied space
+ *   v - size of spin-block virtual space
+ *   d_ov - Delta matrix (size o*v)
+ *   f_ov - Fock matrix (size o*v)
+ *   t1 - CCSD T1 amplitudes (size o*v)
+ *   t2 - CCSD T2 amplitudes, aaaa and abab blocks (size 2*o*o*v*v)
+ *   i_oovo - OOOV integrals transposed, aaaa and abab blocks (size 2*o*o*o*v)
+ *   i_oovv - OOVV integrals, aaaa and abab blocks (size 2*o*o*v*v)
+ *   i_vvov - OVVV integrals transposed, aaaa and abab blocks (size 2*o*v*v*v)
+ *
+ * All arrays should be arranged contiguously in memory by last index first.
+ * E.g., for d_ov the first v contiguous elements in memory are d_ov[o=0,v=0],
+ * d_ov[o=0,v=1], d_ov[o=0,v=2], and so forth. The tensors are expected to
+ * be properly (anti-)symmetrized.
+ *
+ * The function returns CCSD(T) energy correction.
+ *
+ * References:
+ *   J. Chem. Phys. 98, 8718 (1993); http://dx.doi.org/10.1063/1.464480
+ */
+double ccsd_pt(size_t o, size_t v, const double *d_ov, const double *f_ov,
+    const double *t1, const double *t2, const double *i_oovo,
+    const double *i_oovv, const double *i_vvov);
+
+/* Compute CCSD(T) energy correction in parallel for the unrestricted case.
+ *
+ * The routine is MPI/OpenMP parallel. All MPI processes must receive same
+ * input data.
+ *
+ * Arguments:
+ *   o - full size of occupied space
+ *   v - full size of virtual space
  *   d_ov - Delta matrix (size o*v)
  *   f_ov - Fock matrix (size o*v)
  *   t1 - CCSD T1 amplitudes (size o*v)
@@ -47,10 +80,6 @@ extern "C" {
  * References:
  *   J. Chem. Phys. 98, 8718 (1993); http://dx.doi.org/10.1063/1.464480
  */
-double ccsd_pt(size_t o, size_t v, const double *d_ov, const double *f_ov,
-    const double *t1, const double *t2, const double *i_oovo,
-    const double *i_oovv, const double *i_vvov);
-
 double ccsd_upt(size_t o, size_t v, const double *d_ov, const double *f_ov,
     const double *t1, const double *t2, const double *i_oovo,
     const double *i_oovv, const double *i_vvov);
