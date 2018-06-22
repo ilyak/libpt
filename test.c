382bf6526ec9bfd79d5bcb31576eb28c53b4daa6
Ilya Kaliman <ilya.kaliman@gmail.com>
2018-06-22 14:26:21 -0700

    Add mp tests.

--- test.c
+++ test.c
@@ -130,6 +130,58 @@ read_test_data_rpt(FILE *fp, size_t oa, size_t va, double *d_ov, double *f_ov,
 }
 
 static void
+read_test_data_rpt_mp(FILE *fp, size_t oa, size_t va, float *d_ov, float *f_ov,
+    float *t1, float *t2, float *i_oovo, float *i_oovv, float *i_ovvv)
+{
+	size_t i;
+
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*va; i++)
+		d_ov[i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*va; i++)
+		f_ov[i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*va; i++)
+		t1[i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*oa*va*va; i++)
+		t2[i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*oa*va*va; i++)
+		t2[oa*oa*va*va+i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*oa*oa*va; i++)
+		i_oovo[i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*oa*oa*va; i++)
+		i_oovo[oa*oa*oa*va+i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*oa*va*va; i++)
+		i_oovv[i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*oa*va*va; i++)
+		i_oovv[oa*oa*va*va+i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*va*va*(va-1)/2; i++)
+		i_ovvv[i] = read_next_double(fp);
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa*va*va*va; i++)
+		i_ovvv[oa*va*va*(va-1)/2+i] = read_next_double(fp);
+}
+
+static void
 read_test_data_upt(FILE *fp, size_t oa, size_t va, size_t ob, size_t vb,
     double *d_ov, double *f_ov, double *t1, double *t2, double *i_oovo,
     double *i_oovv, double *i_ovvv)
@@ -263,6 +315,170 @@ read_test_data_upt(FILE *fp, size_t oa, size_t va, size_t ob, size_t vb,
 	for (b = 0; b < v; b++)
 	for (c = 0; c < b; c++) {
 		double t = read_next_double(fp);
+		tmp[i*v*v*v+a*v*v+b*v+c] = t;
+		tmp[i*v*v*v+a*v*v+c*v+b] = -t;
+	}
+	for (i = 0; i < o; i++)
+	for (a = 0; a < v; a++)
+	for (b = 0; b < v; b++)
+		tmp[i*v*v*v+a*v*v+b*v+b] = 0;
+	for (i = 0; i < oa; i++)
+	for (a = 0; a < va; a++)
+	for (b = 0; b < va; b++)
+	for (c = 0; c < b; c++)
+		*i_ovvv++ = tmp[i*v*v*v+a*v*v+b*v+c];
+	for (i = 0; i < oa; i++)
+	for (a = 0; a < vb; a++)
+	for (b = 0; b < va; b++)
+	for (c = 0; c < vb; c++)
+		*i_ovvv++ = tmp[i*v*v*v+(a+va)*v*v+b*v+(c+va)];
+	for (i = 0; i < ob; i++)
+	for (a = 0; a < vb; a++)
+	for (b = 0; b < vb; b++)
+	for (c = 0; c < b; c++)
+		*i_ovvv++ = tmp[(i+oa)*v*v*v+(a+va)*v*v+(b+va)*v+(c+va)];
+	for (i = 0; i < ob; i++)
+	for (a = 0; a < va; a++)
+	for (b = 0; b < vb; b++)
+	for (c = 0; c < va; c++)
+		*i_ovvv++ = tmp[(i+oa)*v*v*v+a*v*v+(b+va)*v+c];
+	free(tmp);
+}
+
+static void
+read_test_data_upt_mp(FILE *fp, size_t oa, size_t va, size_t ob, size_t vb,
+    float *d_ov, float *f_ov, float *t1, float *t2, float *i_oovo,
+    float *i_oovv, float *i_ovvv)
+{
+	size_t i, j, k, a, b, c;
+	size_t o = oa+ob;
+	size_t v = va+vb;
+	size_t x = o > v ? o : v;
+	float *tmp;
+
+	tmp = xmalloc(x*x*x*x*sizeof(*tmp));
+
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa; i++) {
+		for (j = 0; j < va; j++)
+			*d_ov++ = read_next_double(fp);
+		for (j = 0; j < vb; j++)
+			read_next_double(fp);
+	}
+	for (i = 0; i < ob; i++) {
+		for (j = 0; j < va; j++)
+			read_next_double(fp);
+		for (j = 0; j < vb; j++)
+			*d_ov++ = read_next_double(fp);
+	}
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa; i++) {
+		for (j = 0; j < va; j++)
+			*f_ov++ = read_next_double(fp);
+		for (j = 0; j < vb; j++)
+			read_next_double(fp);
+	}
+	for (i = 0; i < ob; i++) {
+		for (j = 0; j < va; j++)
+			read_next_double(fp);
+		for (j = 0; j < vb; j++)
+			*f_ov++ = read_next_double(fp);
+	}
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < oa; i++) {
+		for (j = 0; j < va; j++)
+			*t1++ = read_next_double(fp);
+		for (j = 0; j < vb; j++)
+			read_next_double(fp);
+	}
+	for (i = 0; i < ob; i++) {
+		for (j = 0; j < va; j++)
+			read_next_double(fp);
+		for (j = 0; j < vb; j++)
+			*t1++ = read_next_double(fp);
+	}
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < o*o*v*v; i++)
+		tmp[i] = read_next_double(fp);
+	for (i = 0; i < oa; i++)
+	for (j = 0; j < oa; j++)
+	for (a = 0; a < va; a++)
+	for (b = 0; b < va; b++)
+		*t2++ = tmp[i*o*v*v+j*v*v+a*v+b];
+	for (i = 0; i < oa; i++)
+	for (j = 0; j < ob; j++)
+	for (a = 0; a < va; a++)
+	for (b = 0; b < vb; b++)
+		*t2++ = tmp[i*o*v*v+(j+oa)*v*v+a*v+(b+va)];
+	for (i = 0; i < ob; i++)
+	for (j = 0; j < ob; j++)
+	for (a = 0; a < vb; a++)
+	for (b = 0; b < vb; b++)
+		*t2++ = tmp[(i+oa)*o*v*v+(j+oa)*v*v+(a+va)*v+(b+va)];
+	for (i = 0; i < ob; i++)
+	for (j = 0; j < oa; j++)
+	for (a = 0; a < vb; a++)
+	for (b = 0; b < va; b++)
+		*t2++ = tmp[(i+oa)*o*v*v+j*v*v+(a+va)*v+b];
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < o*o*v*o; i++)
+		tmp[i] = read_next_double(fp);
+	for (i = 0; i < oa; i++)
+	for (j = 0; j < oa; j++)
+	for (a = 0; a < va; a++)
+	for (k = 0; k < oa; k++)
+		*i_oovo++ = tmp[i*o*v*o+j*v*o+a*o+k];
+	for (i = 0; i < oa; i++)
+	for (j = 0; j < ob; j++)
+	for (a = 0; a < va; a++)
+	for (k = 0; k < ob; k++)
+		*i_oovo++ = tmp[i*o*v*o+(j+oa)*v*o+a*o+(k+oa)];
+	for (i = 0; i < ob; i++)
+	for (j = 0; j < ob; j++)
+	for (a = 0; a < vb; a++)
+	for (k = 0; k < ob; k++)
+		*i_oovo++ = tmp[(i+oa)*o*v*o+(j+oa)*v*o+(a+va)*o+(k+oa)];
+	for (i = 0; i < ob; i++)
+	for (j = 0; j < oa; j++)
+	for (a = 0; a < vb; a++)
+	for (k = 0; k < oa; k++)
+		*i_oovo++ = tmp[(i+oa)*o*v*o+j*v*o+(a+va)*o+k];
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < o*o*v*v; i++)
+		tmp[i] = read_next_double(fp);
+	for (i = 0; i < oa; i++)
+	for (j = 0; j < oa; j++)
+	for (a = 0; a < va; a++)
+	for (b = 0; b < va; b++)
+		*i_oovv++ = tmp[i*o*v*v+j*v*v+a*v+b];
+	for (i = 0; i < oa; i++)
+	for (j = 0; j < ob; j++)
+	for (a = 0; a < va; a++)
+	for (b = 0; b < vb; b++)
+		*i_oovv++ = tmp[i*o*v*v+(j+oa)*v*v+a*v+(b+va)];
+	for (i = 0; i < ob; i++)
+	for (j = 0; j < ob; j++)
+	for (a = 0; a < vb; a++)
+	for (b = 0; b < vb; b++)
+		*i_oovv++ = tmp[(i+oa)*o*v*v+(j+oa)*v*v+(a+va)*v+(b+va)];
+	for (i = 0; i < ob; i++)
+	for (j = 0; j < oa; j++)
+	for (a = 0; a < vb; a++)
+	for (b = 0; b < va; b++)
+		*i_oovv++ = tmp[(i+oa)*o*v*v+j*v*v+(a+va)*v+b];
+	skip_line(fp);
+	skip_line(fp);
+	for (i = 0; i < o; i++)
+	for (a = 0; a < v; a++)
+	for (b = 0; b < v; b++)
+	for (c = 0; c < b; c++) {
+		float t = read_next_double(fp);
 		tmp[i*v*v*v+a*v*v+b*v+c] = t;
 		tmp[i*v*v*v+a*v*v+c*v+b] = -t;
 	}
@@ -749,6 +965,46 @@ test_rpt(FILE *fp, double *e_ref, double *e_cmp)
 }
 
 static void
+test_rpt_mp(FILE *fp, double *e_ref, double *e_cmp)
+{
+	float *d_ov, *f_ov, *t1, *t2;
+	float *i_oovo, *i_oovv, *i_ovvv;
+	size_t oa, va, ob, vb, d_ov_sz, f_ov_sz, t1_sz, t2_sz;
+	size_t i_oovo_sz, i_oovv_sz, i_ovvv_sz;
+
+	read_test_header(fp, 0, &oa, &va, &ob, &vb, e_ref);
+
+	d_ov_sz = oa*va;
+	f_ov_sz = oa*va;
+	t1_sz = oa*va;
+	t2_sz = oa*oa*va*va + oa*ob*va*vb;
+	i_oovo_sz = oa*oa*va*oa + oa*ob*va*ob;
+	i_oovv_sz = oa*oa*va*va + oa*ob*va*vb;
+	i_ovvv_sz = oa*va*va*(va-1)/2 + oa*vb*va*vb;
+
+	d_ov = xmalloc(d_ov_sz * sizeof(*d_ov));
+	f_ov = xmalloc(f_ov_sz * sizeof(*f_ov));
+	t1 = xmalloc(t1_sz * sizeof(*t1));
+	t2 = xmalloc(t2_sz * sizeof(*t2));
+	i_oovo = xmalloc(i_oovo_sz * sizeof(*i_oovo));
+	i_oovv = xmalloc(i_oovv_sz * sizeof(*i_oovv));
+	i_ovvv = xmalloc(i_ovvv_sz * sizeof(*i_ovvv));
+
+	read_test_data_rpt_mp(fp, oa, va, d_ov, f_ov, t1, t2,
+	    i_oovo, i_oovv, i_ovvv);
+	*e_cmp = libpt_rpt_mp(oa, va, d_ov, f_ov, t1, t2,
+	    i_oovo, i_oovv, i_ovvv);
+
+	free(d_ov);
+	free(f_ov);
+	free(t1);
+	free(t2);
+	free(i_oovo);
+	free(i_oovv);
+	free(i_ovvv);
+}
+
+static void
 test_upt(FILE *fp, double *e_ref, double *e_cmp)
 {
 	double *d_ov, *f_ov, *t1, *t2;
@@ -779,6 +1035,48 @@ test_upt(FILE *fp, double *e_ref, double *e_cmp)
 	read_test_data_upt(fp, oa, va, ob, vb, d_ov, f_ov, t1, t2,
 	    i_oovo, i_oovv, i_ovvv);
 	*e_cmp = libpt_upt(oa, va, ob, vb, d_ov, f_ov, t1, t2,
+	    i_oovo, i_oovv, i_ovvv);
+
+	free(d_ov);
+	free(f_ov);
+	free(t1);
+	free(t2);
+	free(i_oovo);
+	free(i_oovv);
+	free(i_ovvv);
+}
+
+static void
+test_upt_mp(FILE *fp, double *e_ref, double *e_cmp)
+{
+	float *d_ov, *f_ov, *t1, *t2;
+	float *i_oovo, *i_oovv, *i_ovvv;
+	size_t oa, va, ob, vb, d_ov_sz, f_ov_sz, t1_sz, t2_sz;
+	size_t i_oovo_sz, i_oovv_sz, i_ovvv_sz;
+
+	read_test_header(fp, 1, &oa, &va, &ob, &vb, e_ref);
+
+	d_ov_sz = oa*va + ob*vb;
+	f_ov_sz = oa*va + ob*vb;
+	t1_sz = oa*va + ob*vb;
+	t2_sz = oa*oa*va*va + 2*oa*ob*va*vb + ob*ob*vb*vb;
+	i_oovo_sz = oa*oa*va*oa + oa*ob*va*ob + ob*oa*vb*oa +
+	    ob*ob*vb*ob;
+	i_oovv_sz = oa*oa*va*va + 2*oa*ob*va*vb + ob*ob*vb*vb;
+	i_ovvv_sz = oa*va*va*(va-1)/2 + oa*vb*va*vb + ob*va*vb*va +
+	    ob*vb*vb*(vb-1)/2;
+
+	d_ov = xmalloc(d_ov_sz * sizeof(*d_ov));
+	f_ov = xmalloc(f_ov_sz * sizeof(*f_ov));
+	t1 = xmalloc(t1_sz * sizeof(*t1));
+	t2 = xmalloc(t2_sz * sizeof(*t2));
+	i_oovo = xmalloc(i_oovo_sz * sizeof(*i_oovo));
+	i_oovv = xmalloc(i_oovv_sz * sizeof(*i_oovv));
+	i_ovvv = xmalloc(i_ovvv_sz * sizeof(*i_ovvv));
+
+	read_test_data_upt_mp(fp, oa, va, ob, vb, d_ov, f_ov, t1, t2,
+	    i_oovo, i_oovv, i_ovvv);
+	*e_cmp = libpt_upt_mp(oa, va, ob, vb, d_ov, f_ov, t1, t2,
 	    i_oovo, i_oovv, i_ovvv);
 
 	free(d_ov);
@@ -894,26 +1192,39 @@ test_uft(FILE *fp, double *e_ref, double *e_cmp)
 
 static const struct {
 	const char *name;
+	const char *type;
 	void (*fn)(FILE *, double *, double *);
 } tests[] = {
-	{ "rpt01", test_rpt },
-	{ "rpt02", test_rpt },
-	{ "rpt03", test_rpt },
-	{ "rpt04", test_rpt },
-	{ "rpt05", test_rpt },
-	{ "rpt06", test_rpt },
-	{ "rpt07", test_rpt },
-	{ "rpt08", test_rpt },
-	{ "rpt09", test_rpt },
-	{ "upt01", test_upt },
-	{ "upt02", test_upt },
-	{ "upt03", test_upt },
-	{ "rft01", test_rft },
-	{ "rft02", test_rft },
-	{ "rft03", test_rft },
-	{ "uft01", test_uft },
-	{ "uft02", test_uft },
-	{ "uft03", test_uft },
+	{ "rpt01", "double", test_rpt },
+	{ "rpt02", "double", test_rpt },
+	{ "rpt03", "double", test_rpt },
+	{ "rpt04", "double", test_rpt },
+	{ "rpt05", "double", test_rpt },
+	{ "rpt06", "double", test_rpt },
+	{ "rpt07", "double", test_rpt },
+	{ "rpt08", "double", test_rpt },
+	{ "rpt09", "double", test_rpt },
+	{ "upt01", "double", test_upt },
+	{ "upt02", "double", test_upt },
+	{ "upt03", "double", test_upt },
+	{ "rft01", "double", test_rft },
+	{ "rft02", "double", test_rft },
+	{ "rft03", "double", test_rft },
+	{ "uft01", "double", test_uft },
+	{ "uft02", "double", test_uft },
+	{ "uft03", "double", test_uft },
+	{ "rpt01", "mixed", test_rpt_mp },
+	{ "rpt02", "mixed", test_rpt_mp },
+	{ "rpt03", "mixed", test_rpt_mp },
+	{ "rpt04", "mixed", test_rpt_mp },
+	{ "rpt05", "mixed", test_rpt_mp },
+	{ "rpt06", "mixed", test_rpt_mp },
+	{ "rpt07", "mixed", test_rpt_mp },
+	{ "rpt08", "mixed", test_rpt_mp },
+	{ "rpt09", "mixed", test_rpt_mp },
+	{ "upt01", "mixed", test_upt_mp },
+	{ "upt02", "mixed", test_upt_mp },
+	{ "upt03", "mixed", test_upt_mp },
 };
 static const size_t ntests = sizeof(tests) / sizeof(*tests);
 
@@ -935,7 +1246,7 @@ main(int argc, char **argv)
 	for (i = 0; i < ntests; i++) {
 		snprintf(path, sizeof(path), "tests/%s.dat", tests[i].name);
 		if (rank == 0)
-			printf("%s\n", path);
+			printf("%s (%s)\n", path, tests[i].type);
 		if ((fp = fopen(path, "r")) == NULL)
 			err(1, "fopen");
 		tests[i].fn(fp, &e_ref, &e_cmp);
